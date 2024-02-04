55adc8583245499f990dbc84bfe8e5fc076adf5b
sandesh ghoti <sandesh.ghot@gmail.com>
2024-02-04 14:53:07 +0000

    basic setup done and implimented routes and navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
+import { removeUser } from "../utils/userSlice";
+const Navbar = () => {
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleAuth = () => {
+    if (user) {
+      dispatch(removeUser());
+    }
+    navigate("/auth");
+  };
+  return (
+    <div className=" max-wide:max-w-screen-wide mx-auto flex flex-row justify-between items-center bg-gradient-to-b from-black to-white bg-opacity-60 px-3">
+      <NavLink to="/">
+        <div>
+          <img
+            src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+            alt="logo"
+            className="h-16"
+          />
+        </div>
+      </NavLink>
+      <ul className="flex flex-row">
+        <NavLink to="/about">
+          <li className="px-3 text-white">About</li>
+        </NavLink>
+        <li>
+          <button
+            onClick={handleAuth}
+            className="px-3 py-1 bg-red-600 rounded-sm text-sm text-white tracking-tighter"
+          >
+            {!user ? "Sign In" : "Sign Out"}
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-undef */
+export const TMDB_API_KEY = process.env.TMDB_API_KEY;
+export const TMDB_ACCESS_TOKEN = process.env.TMDB_ACCESS_TOKEN;
